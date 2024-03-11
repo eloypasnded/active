3a5015e24706871407e0ddad9931324b3fbf6295
eloypasnded <148542546+eloypasnded@users.noreply.github.com>
2024-03-11 10:33:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-import os
-import telebot
+import requests
+import time
 
-# Obtén el token de tu bot desde BotFather
-BOT_TOKEN = "6993929932:AAFhCHQxVC8_RWO_QMr0GqI-A5k77Kt2S58"  # Reemplaza con tu token real
-bot = telebot.TeleBot(BOT_TOKEN)
+def main():
+    while True:
+        try:
+            start_time = time.time()
+            response1 = requests.get("https://spadl.onrender.com/")
+            response2 = requests.get("https://spadl2.onrender.com/")
+            end_time = time.time()
 
-# Manejador para el comando /active
-@bot.message_handler(commands=['active'])
-def activate_bots(message):
-    try:
-        # Accede a las páginas web
-        web_page1 = "https://spadl.onrender.com/"
-        web_page2 = "https://spadl2.onrender.com/"
+            print(f"Tiempo de respuesta para https://spadl.onrender.com/: {end_time - start_time:.2f} segundos")
+            print(f"Tiempo de respuesta para https://spadl2.onrender.com/: {end_time - start_time:.2f} segundos")
 
-        # Envía un mensaje de activación
-        bot.send_message(message.chat.id, f"Se han activado los bots @hspdlbot y @hspdl2bot. ¡Listos para la acción! 🤖🚀")
-    except Exception as e:
-        bot.send_message(message.chat.id, f"Hubo un error al activar los bots: {str(e)}")
+            # Esperar 10 minutos antes de volver a hacer la solicitud
+            time.sleep(600)
 
-# Manejador para otros comandos o mensajes
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, "¡Comando no reconocido! Usa /active para activar los bots.")
+        except KeyboardInterrupt:
+            print("El script ha sido detenido manualmente.")
+            break
 
-# Inicia el bot
 if __name__ == "__main__":
-    bot.polling()
+    main()
